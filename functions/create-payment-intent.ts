0e28e897ac3b556317017a01079b2b076666a7b9
coding-guy <1codingguy@gmail.com>
2021-12-02 10:27:35 +0700

    env file in gitignore

--- functions/create-payment-intent.ts
+++ functions/create-payment-intent.ts
@@ -1,6 +1,7 @@
 //[domain]/.netlify/functions/create-payment-intent
 
 exports.handler = async function (event, context) {
+  // if there's event.body object then it's a POST request, otherwise it's a GET request
   if (event.body) {
     const { cart, totalAmount } = JSON.parse(event.body)
 
