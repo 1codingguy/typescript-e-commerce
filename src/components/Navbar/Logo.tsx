9f264b1423cd7324348a6b283c73722a6e654983
coding-guy <1codingguy@gmail.com>
2021-12-28 16:56:21 +0700

    changed to a cleaner logo

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import logo from '../../assets/logo_word.jpg'
+// import logo from '../../assets/logo_word.jpg'
+import whiteLogo from '../../assets/logo_white.png'
 import { Link } from 'react-router-dom'
 
 export const Logo = () => {
   return (
     <Link to='/'>
-      <img src={logo} alt='cute buddy' />
+      <img src={whiteLogo} alt='cute buddy' />
     </Link>
   )
 }
